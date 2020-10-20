cca29d2b1c0971a4da3cb6fafa410735f57b5145
vacya01@users.noreply.github.com <vacya01@users.noreply.github.com>
2020-10-20 19:04:41 +0500

    class People realized

--- tools.py
+++ tools.py
@@ -1,9 +1,15 @@
+import os.path
+
+
 class Person:
     def __init__(self, ID, NAME, EMAIL, PHONE):
         self.PHONE = PHONE
         self.EMAIL = EMAIL
         self.NAME = NAME
-        self.ID = ID
+        self.ID = int(ID)
+
+    def __str__(self):
+        return f'{self.ID}, {self.NAME}, {self.EMAIL}, {self.PHONE}'
 
 
 class Gadget:
@@ -11,5 +17,22 @@ class Gadget:
         self.MODEL = MODEL
         self.TYPE = TYPE
         self.BRAND = BRAND
-        self.ID = ID
+        self.ID = int(ID)
 
+
+class People:
+    def __init__(self):
+        if not os.path.exists('people.txt'):
+            print('Файл people.txt не существует')
+        else:
+            f = open('people.txt')
+            self.people = [Person(*record.split(', ')) for record in f.read().split('\n')]
+
+    def __str__(self):
+        result = ''
+        for person in self.people:
+            result += person.__str__() + '\n'
+        return result
+
+
+print(People())
