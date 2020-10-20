0d7f68e7ae7c9264d0bf9e2af28dc146f2a2ea25
vacya01@users.noreply.github.com <vacya01@users.noreply.github.com>
2020-10-20 22:44:55 +0500

    2-4 tasks realized

--- main.py
+++ main.py
@@ -8,4 +8,18 @@ def search_u(text):
         if text in person.NAME or text in person.PHONE:
             f.write(person.__str__() + '\n')
 
-search_u('Vesnin')
+
+def search_e(text):
+    e = Equip()
+    f = open('SEARCH_E.txt', 'w')
+    for gadget in e.gadgets:
+        if text in gadget.MODEL:
+            f.write(gadget.__str__() + '\n')
+
+
+def search_w(text):
+    w = Works()
+    f = open('SEARCH_W.txt', 'w')
+    for work in w.works:
+        if text in work.WORK and not work.WORK.isdigit() or text in str(work.PRICE):
+            f.write(work.__str__() + '\n')
