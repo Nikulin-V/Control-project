5dfc58a457d406e01a67afbb808f943c5cdd168d
vacya01@users.noreply.github.com <vacya01@users.noreply.github.com>
2020-10-20 17:20:29 +0500

    Person and Gadget realized

---
+++ tools.py
@@ -0,0 +1,15 @@
+class Person:
+    def __init__(self, ID, NAME, EMAIL, PHONE):
+        self.PHONE = PHONE
+        self.EMAIL = EMAIL
+        self.NAME = NAME
+        self.ID = ID
+
+
+class Gadget:
+    def __init__(self, ID, BRAND, TYPE, MODEL):
+        self.MODEL = MODEL
+        self.TYPE = TYPE
+        self.BRAND = BRAND
+        self.ID = ID
+
