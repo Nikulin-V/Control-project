a60eb3fde6955d4430bf0507cc6829b5063e5f1f
vacya01@users.noreply.github.com <vacya01@users.noreply.github.com>
2020-10-22 23:40:20 +0500

    All tasks except 5 realized

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import *
+
 from tools import *
 
 
@@ -25,17 +27,15 @@ def search_w(text):
             f.write(work.__str__() + '\n')
 
 
-def new_card(db):
+def new_card(db, line):
     if db == 'USER':
         p = People()
-        line = input('Введите карточку: ')
         new_person = Person(*line.split())
         if new_person.PHONE in p.get_phones() or new_person.ID in p.get_ids():
             raise CardInBaseException('user.txt')
         p.add_person(new_person)
     elif db == 'EQUIP':
         e = Equip()
-        line = input('Введите карточку: ')
         new_gadget = Gadget(*line.split())
         if new_gadget.BRAND in e.get_brands() and new_gadget.MODEL == \
                 e.get_models()[
@@ -44,21 +44,20 @@ def new_card(db):
         e.add_equip(new_gadget)
     elif db == 'WORK':
         w = Works()
-        line = input('Введите карточку: ')
         new_work = Work(*line.split())
         if new_work.WORK in w.get_works() or new_work.ID in w.get_ids():
             raise CardInBaseException('user.txt')
         w.add_work(new_work)
 
 
-def edit_card(db, ID):
+def edit_card(db, ID, line):
     if db == 'USER':
         People().people.pop(People().get_ids().index(ID))
     elif db == 'EQUIP':
         Equip().gadgets.pop(Equip().get_ids().index(ID))
     elif db == 'WORK':
         Works().works.pop(Works().get_ids().index(ID))
-    new_card(db)
+    new_card(db, line)
 
 
 def print_card(db, ID):
@@ -77,3 +76,107 @@ def list_db(db):
         print(Equip())
     elif db == 'WORK':
         print(Works())
+
+
+def create_report():
+    f = open('reports.txt', 'w')
+    lines = []
+    now = datetime.now()
+    for request in Requests().requests:
+        if datetime(*list(map(int, request.COMPLETE_DATE.split('-')))) <= now < datetime(
+                *list(map(int, request.DELIVERY_DATE.split('-')))):
+            email = People().get_emails()[People().get_ids().index(request.CLIENT_ID)]
+            brand = Equip().get_brands()[Equip().get_ids().index(request.EQUIP_ID)]
+            model = Equip().get_models()[Equip().get_ids().index(request.EQUIP_ID)]
+            lines.append(f'{email}, {brand}, {model}, {request.COMPLETE_DATE};\n')
+    f.write('\n'.join([line for line in lines]))
+    f.close()
+
+
+def create_inwork(master_name):
+    f = open('inwork.txt', 'w')
+    now = datetime.now()
+    for request in Requests().requests:
+        if People().get_names()[People().get_ids().index(request.MASTER_ID)] == master_name and \
+                datetime(*list(map(int, request.COMPLETE_DATE.split('-')))) < now:
+            brand = Equip().get_brands()[Equip().get_ids().index(request.EQUIP_ID)]
+            model = Equip().get_models()[Equip().get_ids().index(request.EQUIP_ID)]
+            f.write(f'{request.DATE}, {request.ID}, {brand}, {model}, {request.REASON};\n')
+    f.close()
+
+
+def print_help(command=''):
+    f = open('help.txt', encoding='utf-8')
+    data = f.readlines()
+    if not command:
+        for line in data:
+            print(line.strip('\n'))
+    elif command.startswith('HELP'):
+        print(''.join(data[:2]))
+    elif command.startswith('NEW'):
+        print(data[2])
+        print(''.join(data[9:12]))
+    else:
+        for line in data:
+            if line.startswith(command):
+                print(line.split('\n'))
+                break
+
+
+def del_request(ID):
+    Requests().requests.pop(Requests().get_ids().index(ID))
+
+
+def new_request():
+    pass
+
+
+def main():
+    command = input().split()
+    if command[0] == 'HELP':
+        if len(command) == 1:
+            print_help()
+        elif len(command) == 2:
+            print_help(command[1])
+    elif command[0] == 'NEW':
+        if len(command) == 3:
+            new_card(command[1], command[2])
+        elif command[1] == 'REQUEST':
+            new_request()
+    elif command[0] == 'LIST' and len(command) == 2:
+        list_db(command[1])
+    elif command[0] == 'EDIT' and len(command) == 4:
+        edit_card(int(command[2]), command[1], command[3])
+    elif command[0] == 'PRINT' and len(command) == 3:
+        print_card(command[2], int(command[1]))
+    elif command[0] == 'FIND_U' and len(command) == 2:
+        search_u(command[1])
+    elif command[0] == 'FIND_E' and len(command) == 2:
+        search_u(command[1])
+    elif command[0] == 'FIND_W' and len(command) == 2:
+        search_u(command[1])
+    elif command[0] == 'DEL' and len(command) == 2:
+        del_request(int(command[1]))
+    elif command[0] == 'EXIT' and len(command) == 1:
+        raise ExitException
+    else:
+        raise UnknownCommandException(f"'{' '.join(command)}'")
+
+
+if __name__ == '__main__':
+    while True:
+        try:
+            main()
+        except CardInBaseException as file_name:
+            print(f'Карточка уже есть в {file_name}')
+        except UnknownCommandException as command_line:
+            print(f"Неизвестная команда {command_line}")
+        except ExitException:
+            print('Выход...')
+            break
+        except TypeError:
+            print('Неизвестная команда')
+        except Exception:
+            print(Exception.__class__.__name__)
+        finally:
+            print('----------------------------------------------\n')
